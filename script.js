05abded921fef7105ebc1e0ce4d9db5a89bba390
MarquesMiguel <miguelmcplacido@gmail.com>
2026-06-25 01:07:16 -0300

    started the eye class

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+let canvas;
+let ctx;
 window.onload = function(){
-    const canvas = document.getElementById('background');
+    canvas = document.getElementById('background');
     const ctx = canvas.getContext('2d');
     
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+}
+
+// responsive canvas size 
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+});
+
+class Eye{
+    #ctx;
+    #width;
+    #heigth
+
+    constructor(ctx, width, height){
+        this.#ctx = ctx;
+        this.#width = width;
+        this.#heigth = height;
+    }
+
+    #draw_eye(){
+
+    }
 
 }
