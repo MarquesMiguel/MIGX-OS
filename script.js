088cfc2b5965f7d545e9ac34138147ec4413c395
MarquesMiguel <miguelmcplacido@gmail.com>
2026-06-24 23:43:04 -0300

    create script.js file,  initial setup to the canvas and restart css config

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    const canvas = document.getElementById('background');
+    const ctx = canvas.getContext('2d');
+    
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+}
